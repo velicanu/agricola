9b42511c5baddeedd2524d90089316a5ad7bfe0d
Dragos Velicanu <dragos@velicanu.com>
2018-07-28 20:35:36 -0500

    added minor improvements code

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@ import csv
 import operator
 import sys
 import numpy as np
-# import pandas as pd
 
-decks = sys.argv[2:]
+if(len(sys.argv)<6):
+    print("usage:")
+    print("python3 setup.py <nplayers> <nocc> <nmip> <eik> <e1 e3 ...>")
+    exit(1)
 players = int(sys.argv[1])
+nocc = int(sys.argv[2])
+nmip = int(sys.argv[3])
+mipdecks = list(sys.argv[4])
+decks = sys.argv[5:]
 occupations = []
-# both = [x in a for x in b]
+minorimp = []
 
-# print(sys.argv)
+# randperm occ deck
 draw = []
 with open('occupation.csv') as csvfile:
     occupations_ = csv.reader(csvfile)
@@ -17,26 +23,42 @@ with open('occupation.csv') as csvfile:
         if(row and row[0] in decks):
             draw += row[1:]
             occupations.append(row)
-        # print(row)
-        # print(np.random.permutation(row[1:]))
-        # if("216" in row):
-            # print(row[0],"contains",216)
 draw = np.random.permutation(draw)
-hand = []
 
+# randperm mip deck
+mipdraw = []
+with open('minor-improvements.csv') as csvfile:
+    minorimp_ = csv.reader(csvfile)
+    for row in minorimp_:
+        if(row and row[0] in mipdecks):
+            mipdraw += row[1:]
+            minorimp.append(row)
+mipdraw = np.random.permutation(mipdraw)
+
+# draw cards for each player
+hand = []
+miphand = []
 for player in range(players):
-    print("Player",player+1,"draw occupation cards:")
-    for card in draw[7*player:7*(player+1)]:
+    print("P",player+1,"  occup  minor imp")
+    for card in draw[nocc*player:nocc*(player+1)]:
         which = ""
         for row in occupations:
             if(card in row):
                 which = row[0]
         hand.append([which,card])
     shand = sorted(hand, key=operator.itemgetter(0,1))
-    print(np.matrix(shand))
+    # print(np.matrix(shand))
     hand.clear()
-# print(hand)
-# print(shand)
-# print(draw)
-# print(draw[0:7])
-# print(draw[7:14])
+    # print("Player",player+1,"draw minor improvement cards:")
+    for card in mipdraw[nmip*player:nmip*(player+1)]:
+        which = ""
+        for row in minorimp:
+            if(card in row):
+                which = row[0]
+        miphand.append([which,card])
+    smiphand = sorted(miphand, key=operator.itemgetter(0,1))
+    # print(np.matrix(smiphand))
+    print(np.concatenate((np.matrix(shand),np.matrix(smiphand)),1))
+    hand.clear()
+    miphand.clear()
+    
